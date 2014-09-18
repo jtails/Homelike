35c3c108aeb4854dbf6e30211f9e5b7db4728fe9
GrzegorzFeather <GrzegorzFeathers@Jorges-MacBook-Pro.local>
2014-09-17 22:23:17 -0500

    Arregla bug en botón agregar dirección
    Arregla el layout de mis direcciones para mostrar correctamente el
    botón de agregar dirección

--- Android/Homelike/app/src/main/java/mx/jtails/homelike/ui/MyAddressesActivity.java
+++ Android/Homelike/app/src/main/java/mx/jtails/homelike/ui/MyAddressesActivity.java
@@ -4,23 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 import mx.jtails.homelike.R;
 import mx.jtails.homelike.api.model.Direccion;
 import mx.jtails.homelike.api.model.Servicio;
 import mx.jtails.homelike.model.provider.HomelikeDBManager;
-import mx.jtails.homelike.ui.adapter.MyAddressesAdapter;
 import mx.jtails.homelike.ui.widget.TrackableScrollView;
 
 public class MyAddressesActivity extends ActionBarActivity
-    implements AdapterView.OnItemClickListener, TrackableScrollView.Callbacks {
+    implements TrackableScrollView.Callbacks {
 
     public static final String ARG_REQUESTED_SERVICE_ID = "requested_service_id";
 
@@ -29,8 +31,7 @@ public class MyAddressesActivity extends ActionBarActivity
 
     private TrackableScrollView mScrollView;
     private View mHeader;
-    private ListView mListViewAddresses;
-    private MyAddressesAdapter mMyAddressesAdapter;
+    private ViewGroup mLayoutAddresses;
 
     private List<Direccion> mAddresses = HomelikeDBManager.getDBManager().loadAddresses();
 
@@ -41,7 +42,7 @@ public class MyAddressesActivity extends ActionBarActivity
 
         this.setupActionBar();
 
-        if(savedInstanceState == null){
+        if (savedInstanceState == null) {
             this.loadService(this.getIntent().getExtras());
         } else {
             this.loadService(savedInstanceState);
@@ -49,27 +50,23 @@ public class MyAddressesActivity extends ActionBarActivity
 
         this.mScrollView = (TrackableScrollView) this.findViewById(R.id.scroll_view);
         this.mHeader = this.findViewById(R.id.layout_header);
-        this.mListViewAddresses = (ListView) this.findViewById(R.id.list_addresses);
-        this.mMyAddressesAdapter = new MyAddressesAdapter(this.getApplicationContext(),
-            this.mAddresses);
+        this.mLayoutAddresses = (ViewGroup) this.findViewById(R.id.layout_addresses);
 
         this.mScrollView.addCallbacks(this);
-        this.mListViewAddresses.setOnItemClickListener(this);
-        this.mListViewAddresses.setAdapter(this.mMyAddressesAdapter);
         this.onScrollChanged(0, 0);
     }
 
-    private void loadService(Bundle args){
+    private void loadService(Bundle args) {
         this.mServiceId = args.getInt(ARG_REQUESTED_SERVICE_ID);
         this.mService = HomelikeDBManager.getDBManager().getService(this.mServiceId);
-        if(this.mService == null){
+        if (this.mService == null) {
             Toast.makeText(this, "Failed to recover address with id: " + this.mServiceId,
                     Toast.LENGTH_SHORT).show();
             this.finish();
         }
     }
 
-    private void setupActionBar(){
+    private void setupActionBar() {
         ActionBar ab = this.getSupportActionBar();
 
         ab.setDisplayShowHomeEnabled(true);
@@ -93,22 +90,58 @@ public class MyAddressesActivity extends ActionBarActivity
     protected void onResume() {
         super.onResume();
         this.mAddresses = HomelikeDBManager.getDBManager().loadAddresses();
-        this.mMyAddressesAdapter.updateContent(this.mAddresses);
-        this.mMyAddressesAdapter.notifyDataSetChanged();
+        this.updateContent();
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    private void updateContent() {
+        this.mLayoutAddresses.removeAllViews();
+        int count = this.mAddresses.size();
+        for (int i = 0 ; i < count ; i++) {
+            final View rowView = LayoutInflater.from(this).inflate(
+                    R.layout.list_item_address, this.mLayoutAddresses, false);
+            ViewHolder holder = new ViewHolder(rowView);
+            rowView.setTag(holder);
+
+            Direccion a = this.mAddresses.get(i);
+            holder.lblAlias.get().setText(a.getReferencia2());
+            holder.lblAddress.get().setText(
+                    a.getCalle() + " #" + a.getNexterior() + ", "
+                            + a.getColonia() + ", " + a.getDelegacion());
+            holder.imgBookmark.get().setVisibility(
+                    a.getEsDefault() == 1 ? View.VISIBLE : View.GONE);
+
+            final int position = i;
+            rowView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClick(rowView, position);
+                }
+            });
+
+            this.mLayoutAddresses.addView(rowView);
+        }
+
+        final View addAddressView = LayoutInflater.from(this).inflate(
+                R.layout.list_item_new_address, this.mLayoutAddresses, false);
+        addAddressView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClick(addAddressView, -1);
+            }
+        });
+        this.mLayoutAddresses.addView(addAddressView);
+    }
+
+    public void onItemClick(View view, int position) {
         Bundle args = new Bundle();
-        if(position == this.mMyAddressesAdapter.getCount() - 1) {
+        if(position < 0) {
             Intent intent = new Intent(this, AddressActivity.class);
             args.putString(AddressActivity.ARG_ADDRESS_MODE, AddressActivity.MODE_REGISTER_ADDRESS);
             intent.putExtras(args);
             this.startActivity(intent);
         } else {
-            Direccion address = this.mMyAddressesAdapter.getItem(position);
+            Direccion address = this.mAddresses.get(position);
             Intent intent = new Intent(this, CheckOrderActivity.class);
-
             args.putInt(CheckOrderActivity.ARG_ADDRESS_ID, address.getIdDireccion());
             args.putInt(CheckOrderActivity.ARG_SERVICE_ID, this.mServiceId);
             intent.putExtras(args);
@@ -127,4 +160,22 @@ public class MyAddressesActivity extends ActionBarActivity
         int scrollY = this.mScrollView.getScrollY();
         this.mHeader.setTranslationY(scrollY * 0.5f);
     }
+
+    private class ViewHolder {
+
+        private WeakReference<TextView> lblAlias;
+        private WeakReference<TextView> lblAddress;
+        private WeakReference<ImageView> imgBookmark;
+
+        public ViewHolder(View view) {
+            this.lblAlias = new WeakReference<TextView>(
+                    (TextView) view.findViewById(R.id.lbl_alias));
+            this.lblAddress = new WeakReference<TextView>(
+                    (TextView) view.findViewById(R.id.lbl_resumed_address));
+            this.imgBookmark = new WeakReference<ImageView>(
+                    (ImageView) view.findViewById(R.id.img_bookmark));
+        }
+
+    }
+
 }
