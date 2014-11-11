8d57879f86f07a7adc8c8526e54c8505e168e0af
Jorge Mendez <jmendez.garmendia@gmail.com>
2014-11-10 20:38:56 -0600

    Únicamente una llamada para actualizar orden

--- Android/Homelike/app/src/main/java/mx/jtails/homelike/request/DeleteAddressRequest.java
+++ Android/Homelike/app/src/main/java/mx/jtails/homelike/request/DeleteAddressRequest.java
@@ -1,13 +1,7 @@
 package mx.jtails.homelike.request;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import mx.jtails.homelike.api.endpoint.cuentaendpoint.Cuentaendpoint;
-import mx.jtails.homelike.api.model.Cuenta;
 import mx.jtails.homelike.api.model.Direccion;
-import mx.jtails.homelike.api.model.Dispositivo;
-import mx.jtails.homelike.util.HomelikePreferences;
 
 /**
  * Created by GrzegorzFeathers on 11/6/14.
@@ -25,26 +19,8 @@ public class DeleteAddressRequest extends HomelikeApiRequest {
 
     @Override
     protected Object doRequest() throws Exception {
-        Cuenta account = new Cuenta();
-        account.setIdCuenta(HomelikePreferences.loadInt(
-                HomelikePreferences.ACCOUNT_ID, -1));
-
-        Dispositivo device = new Dispositivo();
-        device.setIdDispositivo(HomelikePreferences.loadInt(
-                HomelikePreferences.DEVICE_ID, -1));
-
-        account.setDirecciones(new ArrayList<Direccion>(Arrays.asList(
-                new Direccion().setIdDireccion(this.mAddress.getIdDireccion()))));
-        //account.setDispositivos(new ArrayList<Dispositivo>(Arrays.asList(device)));
-
-        Cuentaendpoint.DeleteDireccion request =
-                ((Cuentaendpoint) this.mEndpoint).deleteDireccion(this.mAddress.getIdDireccion());
-        //String jsonString = request.getJsonContent().toString();
-        System.out.println(request.toString());
-        //System.out.println(jsonString);
-        System.out.println(request.buildHttpRequestUrl());
-
-        return request.execute();
+        return ((Cuentaendpoint) this.mEndpoint)
+                .deleteDireccion(this.mAddress.getIdDireccion()).execute();
     }
 
     @Override
