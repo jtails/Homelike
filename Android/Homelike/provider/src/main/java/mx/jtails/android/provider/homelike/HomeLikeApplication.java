0db9e2d63099c2db62eace10dc8794ca22f89250
Jorge Mendez <jmendez.garmendia@gmail.com>
2014-11-18 19:26:39 -0600

    Creación del proyecto para proveedor

---
+++ Android/Homelike/provider/src/main/java/mx/jtails/android/provider/homelike/HomeLikeApplication.java
@@ -0,0 +1,9 @@
+package mx.jtails.android.provider.homelike;
+
+import android.app.Application;
+
+/**
+ * Created by GrzegorzFeathers on 11/18/14.
+ */
+public class HomeLikeApplication extends Application {
+}
---
+++ Android/Homelike/provider/src/main/java/mx/jtails/android/provider/homelike/ui/SplashActivity.java
@@ -0,0 +1,9 @@
+package mx.jtails.android.provider.homelike.ui;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by GrzegorzFeathers on 11/18/14.
+ */
+public class SplashActivity extends ActionBarActivity {
+}
