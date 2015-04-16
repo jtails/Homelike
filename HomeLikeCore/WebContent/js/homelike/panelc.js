4cb22972b7b479ec9bb4b009fc35642bbaf7c79f
jtails <ramses.bernaldez@gmail.com>
2015-04-15 23:58:04 -0500

    Limpieza en proyecto

--- HomeLikeCore/WebContent/js/homelike/panelc.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#menupedidos").click(function(){
-		$("#contenido").load("fragments/pedidosc.jsp");
-	});
-	$("#menuhpedidos").click(function(){
-		$("#contenido").load("fragments/hpedidosc.jsp");
-	});
-});
